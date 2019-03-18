799128bc9ea09e9f87e8cf57f65260a1dd996bfe
Ewa Pojasek <46863741+EwaPojasek@users.noreply.github.com>
2019-03-18 20:46:39 +0100

    Update urls.py

--- school/school/urls.py
+++ school/school/urls.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.conf import settings
-from django.urls import include, path
-
-urlpatterns = [path("admin/", admin.site.urls)]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-=======
 from django.urls import include, path
 from . import views
 
@@ -21,6 +9,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-
->>>>>>> 076a63693524f5a29baf8d056918270c9b0d3ae4
     ] + urlpatterns
