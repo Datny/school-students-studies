51ba4361edddc7e21bc840fd59c2cce3a047dff4
EwaPojasek <pojasek.ewa@gmail.com>
2019-03-20 17:37:30 +0100

    Fix debug problem

--- school/school/urls.py
+++ school/school/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+
 from . import views
 
 urlpatterns = [path("admin/", admin.site.urls),
-               path("", views.home),
+               path("", views.home),]
                
 if settings.DEBUG:
     import debug_toolbar
