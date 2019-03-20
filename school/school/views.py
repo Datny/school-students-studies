d8691313815052c3d7244a99e29df60223d0e32d
EwaPojasek <pojasek.ewa@gmail.com>
2019-03-20 21:25:20 +0100

    Create teacher list view

--- school/school/views.py
+++ school/school/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Teacher
 
 
 def home(request):
     return render(request, "home.html")
+
+def teachers(request):
+    teachers = Teacher.objects.order_by("name")
+    return render(request, "teachers.html", {"teachers": teachers})
