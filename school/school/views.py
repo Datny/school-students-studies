ce5231304fe5c37d1e76ce18a51730487f1b7f74
EwaPojasek <pojasek.ewa@gmail.com>
2019-03-22 17:48:44 +0100

    Create template students list

--- school/school/views.py
+++ school/school/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Teacher
+from .models import Student
 
 
 def home(request):
@@ -8,3 +9,7 @@ def home(request):
 def teachers(request):
     teachers = Teacher.objects.order_by("name")
     return render(request, "teachers.html", {"teachers": teachers})
+
+def students(request):
+    students = Student.objects.order_by("name")
+    return render(request, "students.html", {"students": students})
