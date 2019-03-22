8c90b99fc17ef420ef11c70be6f2171eb6209f54
Ewa Pojasek <46863741+EwaPojasek@users.noreply.github.com>
2019-03-22 18:04:20 +0100

    Create view students list (#28)
    * Create teachers template
    
    * Create teacher list view
    
    * Create template students list

--- school/school/urls.py
+++ school/school/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 
 from . import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home), path ("teachers/", views.teachers)]
+urlpatterns = [path("admin/", admin.site.urls), path("", views.home), 
+                path ("teachers/", views.teachers), 
+                path ("students/", views.students)]
 
 if settings.DEBUG:
     import debug_toolbar
