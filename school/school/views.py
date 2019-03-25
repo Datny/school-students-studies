3c961db23857f04c730fffa26a1a351519887052
datny <robert.przydatek.cl@gmail.com>
2019-03-25 20:43:38 +0100

    refreshing page with notnull form also adds record to db

--- school/school/views.py
+++ school/school/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Teacher
 from .models import Student
+from .forms import AddTeacherForm
+
 
 def home(request):
     return render(request, "home.html")
@@ -8,10 +10,18 @@ def home(request):
 
 def teachers(request):
     teachers = Teacher.objects.order_by("name")
-    return render(request, "teachers.html", {"teachers": teachers})
+    form = AddTeacherForm(request.POST)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+    else:
+        form = AddTeacherForm()
+    return render(request, 'teachers.html', {"form": form, "teachers": teachers})
 
 
 def students(request):
     students = Student.objects.order_by("name")
     return render(request, "students.html", {"students": students})
+
+
+
 
