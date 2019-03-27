346a10485280d2c586192c7bf905dc05e28a3726
amaci <maciolekandrzej@gmail.com>
2019-03-27 17:52:29 +0100

    add form and view

--- school/school/views.py
+++ school/school/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Teacher
 from .models import Student
+from .forms import StudentForm
 from django import forms
 
 
@@ -15,6 +16,14 @@ def teachers(request):
 
 def students(request):
     students = Student.objects.order_by("name")
+
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("Student")
+    else:
+        form = StudentForm()
     return render(request, "students.html", {"students": students})
 
 
