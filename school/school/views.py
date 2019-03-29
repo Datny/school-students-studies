0b20a4dd785f21c007c7e370b9dc69777b4ad98a
Robert Przydatek <42434572+Datny@users.noreply.github.com>
2019-03-29 19:35:20 +0100

    fix debug toolbar (#55)

--- school/school/views.py
+++ school/school/views.py
@@ -22,7 +22,7 @@ def teachers(request):
             return redirect("teachers")
     else:
         form = AddTeacherForm()
-    return render(request, "teachers.html", {"form": form, "teachers": teachers})
+    return render(request, "teachers.html", {"form": form, "teachers": teachers},)
 
 
 def students(request):
