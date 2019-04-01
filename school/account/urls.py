2edf07c873811b83b8370c7084ac9b2914582292
amaci <maciolekandrzej@gmail.com>
2019-04-01 16:41:49 +0200

    changes in db
    db changes
    
    db changes
    
    add login and signup panel
    
    change home.html
    
    final changes
    
    add spaces

--- school/account/urls.py
+++ school/account/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
-from django.urls import re_path
+from django.urls import re_path, path, include
 
 
-urlpatterns = [re_path(r"^login/$", views.user_login, name="login")]
+urlpatterns = [
+    re_path(r"^login/$", views.user_login, name="login"),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
+
+]
