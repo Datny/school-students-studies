9fe95072c1e04bbff985df24d2c281e65358ddb2
datny <robert.przydatek.cl@gmail.com>
2019-04-03 18:30:44 +0200

    1st change of css

--- school/school/settings.py
+++ school/school/settings.py
@@ -115,7 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = [
+#os.path.join(BASE_DIR, 'school/static/')
+#]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 
 
