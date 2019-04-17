88964a7d3ad0ee485acbaa4389be6ea34d75e3ba
datny <robert.przydatek.cl@gmail.com>
2019-04-17 11:12:22 +0200

    create email invitation form and registered in admin panel

--- school/account/forms.py
+++ school/account/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from .models import Invite
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class InviteForm(forms.Form):
+    class Meta:
+        model = Invite
+        fields = ('email',)
--- school/account/models.py
+++ school/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Invite(models.Model):
+    email = models.EmailField()
+    sent_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
