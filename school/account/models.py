6070f29b425bafcab608f760c1103e5162c38d65
datny <robert.przydatek.cl@gmail.com>
2019-04-17 12:49:29 +0200

    send email - no token

--- school/account/models.py
+++ school/account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Invite(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     sent_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
