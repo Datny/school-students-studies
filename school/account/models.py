6b10783168402cc5849bdf8907691fea2aba1b29
datny <robert.przydatek.cl@gmail.com>
2019-04-19 11:41:35 +0200

    sending registration token by email

--- school/account/models.py
+++ school/account/models.py
@@ -4,6 +4,16 @@ from django.db import models
 class Invite(models.Model):
     email = models.EmailField(unique=True)
     sent_date = models.DateTimeField(auto_now_add=True)
+    reg_token = models.TextField(max_length=50, default="")
+    reg_token_valid = models.BooleanField(default=True)
+
+#reg_token_valid will be set to True, after account creation or/if few days pass by
+
+#token is hash generated for each email for registration purposes
+
+
+
+
 
     def __str__(self):
         return self.email
--- school/account/urls.py
+++ school/account/urls.py
@@ -3,12 +3,13 @@ from . import views
 from django.urls import re_path, path, include
 
 
-# path("account/", include("account.urls")),
 
+#ALL PATHS BELLOW ARE ACCESIBLE ONLY AFTER /account/
 urlpatterns = [
     re_path(r"^login/$", views.user_login, name="login"),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('invite', views.invite, name='invite')
+    path('invite', views.invite, name='invite'),
+
 ]
