88cc25516384ce61d4d3033255de18ece721eefe
amaci <maciolekandrzej@gmail.com>
2019-04-24 14:46:12 +0200

    change in log_in form

--- school/account/urls.py
+++ school/account/urls.py
@@ -7,8 +7,6 @@ from django.urls import re_path, path, include
 #ALL PATHS BELLOW ARE ACCESIBLE ONLY AFTER /account/
 urlpatterns = [
     re_path(r"^login/$", views.user_login, name="login"),
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('invite', views.invite, name='invite'),
     path('invites', views.email_invitations, name='csvupload'),
--- school/account/views.py
+++ school/account/views.py
@@ -16,7 +16,9 @@ from django.core.validators import validate_email
 
 
 
+
 def user_login(request):
+    error = None
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -25,42 +27,15 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse("authentication success")
+                    return HttpResponse("authentication success") #redirect to home
                 else:
-                    return HttpResponse("Account is blocked")
+                    error = "Account is blocked"
             else:
-                return HttpResponse("Bad username or password")
+                error = "Bad username or password"
     else:
         form = LoginForm()
+    return render(request, "account/login.html", {"form": form, "error": error})
     return render(request, "account/login.html", {"form": form})
-
-
-def signup(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username=request.POST["username"])
-                return render(request, 'account/signup.html', {'error': 'Username has already been taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(request.POST["username"], password=request.POST['password1'])
-                auth.login(request, user)
-                return redirect('home')
-        else:
-            return render(request, 'account/signup.html', {'error': 'Passwords must match'})
-    else:
-        return render(request, 'account/signup.html')
-
-
-def login(request):
-    if request.method == 'POST':
-        user = auth.authenticate(username=request.POST["username"], password=request.POST["password"])
-        if user is not None:
-            auth.login(request, user)
-            return redirect('home')
-        else:
-            return render(request, 'account/login.html', {'error': 'username or password is incorrect.'})
-    else:
-        return render(request, 'account/login.html')
 
 
 def logout(request):
