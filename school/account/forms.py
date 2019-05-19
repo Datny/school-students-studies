862dff0041cc32f784a78f067c2f6ab77d7025ed
datny <robert.przydatek.cl@gmail.com>
2019-05-19 08:57:11 +0200

    sms send with number auth

--- school/account/forms.py
+++ school/account/forms.py
@@ -1,5 +1,8 @@
-from django import forms
 from .models import Invite
+from django import forms
+from django.core import validators
+import re
+
 
 
 class LoginForm(forms.Form):
@@ -13,9 +16,18 @@ class InviteForm(forms.ModelForm):
         fields = ('email',)
 
 
+
+def check_if_phone_number(value):
+    pattern ="^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$"
+    match = re.search(pattern, value)
+
+    if match == None:
+        raise forms.ValidationError("This is not valid phone number")
+
+
 class SendSmsForm(forms.Form):
     sms_text = forms.CharField(max_length=155,widget=forms.Textarea)
-    reciver = forms.CharField(max_length=12)
+    reciver = forms.CharField(max_length=12, validators=[check_if_phone_number])
 
 
 
