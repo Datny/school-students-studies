ab347adf16b85541a19ffd27b847c70eccde391c
Robert Przydatek <42434572+Datny@users.noreply.github.com>
2019-05-20 16:35:40 +0200

    Send sms (#99)
    * emails not duplicated in invite and csv models
    
    * mass emails 1/2
    
    * del print
    
    * 2nd commit
    
    * create sender form
    
    * send sms to console no valid
    
    * sms send with number auth
    
    * add requirements.txt
    
    * add crispy

--- school/account/forms.py
+++ school/account/forms.py
@@ -1,5 +1,8 @@
-from django import forms
 from .models import Invite
+from django import forms
+from django.core import validators
+import re
+
 
 
 class LoginForm(forms.Form):
@@ -11,3 +14,23 @@ class InviteForm(forms.ModelForm):
     class Meta:
         model = Invite
         fields = ('email',)
+
+
+
+def check_if_phone_number(value):
+    pattern ="^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$"
+    match = re.search(pattern, value)
+
+    if match == None:
+        raise forms.ValidationError("This is not valid phone number")
+
+
+class SendSmsForm(forms.Form):
+    sms_text = forms.CharField(max_length=155,widget=forms.Textarea)
+    reciver = forms.CharField(max_length=12, validators=[check_if_phone_number])
+
+
+
+
+
+
